faeb7f5898ee7d69c418bd78240bee29071544ea
Komoire Ashiraf <127754423+komoire@users.noreply.github.com>
2024-10-09 10:31:18 +0300

    Add files via upload

---
+++ HTML lessons/Script.js
@@ -0,0 +1,16 @@
+// JavaScript (JS) is a programming language that makes websites interactive and dynamic. It is primarily used in web browsers to create dropdown menus, form validation, interactive maps, and other elements on a website.
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+    </head>
+    <body>
+        <button onclick="displayAlert()">Click me!</button>
+
+        <script>
+          function displayAlert() {
+            alert("Learning coding at midnight but i know one day it will pay off");
+          }
+        </script>
+    </body>
+</html>
